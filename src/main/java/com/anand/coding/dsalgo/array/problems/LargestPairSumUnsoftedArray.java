82318331b5437476f319b18e3fdfd42f7ae18ba9
Bikas Anand <anand.iitr.mca@gmail.com>
2019-08-15 20:48:17 +0530

    Sum of the first and second largest value in an array

---
+++ src/main/java/com/anand/coding/dsalgo/array/problems/LargestPairSumUnsoftedArray.java
@@ -0,0 +1,55 @@
+package com.anand.coding.dsalgo.array.problems;
+
+/**
+ * sum of the first and second largest value in an array.
+ *
+ * Complexity: O(n)
+ */
+public class LargestPairSumUnsoftedArray {
+
+
+    /**
+     *
+     * @param A
+     * @return
+     */
+    public static int sumOfLargestPair(int A[]){
+
+        if(A.length<2){
+            return Integer.MIN_VALUE;
+        }
+
+        int first, second;
+
+        if(A[0]>A[1]){
+            first = A[0];
+            second = A[1];
+        } else {
+            first = A[1];
+            second = A[0];
+        }
+
+        for(int i=2; i<A.length; i++){
+            if(A[i]>first){
+                second = first;
+                first = A[i];
+            } else if(A[i]>second && A[i] != first){
+                second = A[i];
+            }
+        }
+
+        return first + second;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String []args){
+
+        int A[] = {3,4,2,9,5,2,8,0};
+
+        System.out.println(sumOfLargestPair(A));
+
+    }
+}
