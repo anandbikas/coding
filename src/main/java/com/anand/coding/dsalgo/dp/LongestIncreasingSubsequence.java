3ee9a54e21069c26f69db2a65d6159c7997d026d
Bikas Anand <anand.iitr.mca@gmail.com>
2019-09-11 20:47:29 +0530

    DP LIS: Find longest increasing sub sequence from an array of integers

---
+++ src/main/java/com/anand/coding/dsalgo/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,62 @@
+package com.anand.coding.dsalgo.dp;
+
+/**
+ * Find longest increasing sub sequence from an array of integers.
+ *
+ */
+public class LongestIncreasingSubsequence {
+
+
+    public static int lis(int []A){
+
+        int n = A.length;
+        if(n<=1){
+            return n;
+        }
+
+        int [] DP = new int[n];
+        int max = 0;
+        for(int i=0; i<n; i++){
+            DP[i]=1;
+            for(int j=i-1; j>=0; j--){
+                if(A[j]<=A[i] && DP[j]+1>=DP[i]){
+                    DP[i] = DP[j]+1;
+                }
+            }
+            if(DP[i]>max){
+                max = DP[i];
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.print(DP[i] + "    ");
+        }
+        System.out.println();
+
+        //Print sequence
+        int value = max;
+        while(--n>=0){
+            if(DP[n]==value){
+                System.out.print(A[n] + "    ");
+                value--;
+            }
+        }
+        System.out.println();
+
+        return max;
+    }
+
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String []args){
+
+        int A[] = {10, 20, 9, 33, 21, 50, 41, 60, 80,1};
+
+        System.out.println(lis(A));
+
+
+    }
+}
