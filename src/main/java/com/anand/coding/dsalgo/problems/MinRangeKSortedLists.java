4fe393bf9dcb121ffff0d1264208baa6d92330af
Bikas Anand <anand.iitr.mca@gmail.com>
2019-11-01 19:14:57 +0530

    MinRangeKSortedLists using Heap.

--- src/main/java/com/anand/coding/dsalgo/problems/MinRangeKSortedLists.java
+++ src/main/java/com/anand/coding/dsalgo/problems/MinRangeKSortedLists.java
@@ -1,5 +1,8 @@
 package com.anand.coding.dsalgo.problems;
 
+import com.anand.coding.dsalgo.graph.adjacencylist.Pair;
+import com.anand.coding.dsalgo.tree.arraybased.BinaryMinHeap;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,27 +25,22 @@ public class MinRangeKSortedLists {
 
         int [] kPointers = new int[list.size()];
 
+        int range,min,max;
 
-        int range;
-        int min;
-        int max;
+        int minValueArrayIndex = 0;
+        min = max = list.get(0)[0];
 
-        int minArrayIndex;
+        for(int i=1; i<list.size(); i++){
+            if(list.get(i)[0] < min){
+                min = list.get(i)[0];
+                minValueArrayIndex = i;
+            } else if(list.get(i)[0] > max){
+                max = list.get(i)[0];
+            }
+        }
+
 
         while(true){
-
-            minArrayIndex = 0;
-            min = max = list.get(minArrayIndex)[kPointers[minArrayIndex]];
-
-            for(int i=1; i<list.size(); i++){
-                if(list.get(i)[kPointers[i]] < min){
-                    min = list.get(i)[kPointers[i]];
-                    minArrayIndex = i;
-                } else if(list.get(i)[kPointers[i]] > max){
-                    max = list.get(i)[kPointers[i]];
-                }
-            }
-
             range = max-min;
             if(minRange > range){
                 minRange = range;
@@ -50,10 +48,21 @@ public class MinRangeKSortedLists {
                 minRangeMax = max;
             }
 
-            kPointers[minArrayIndex]++;
-
-            if( kPointers[minArrayIndex] >= list.get(minArrayIndex).length){
+            kPointers[minValueArrayIndex]++;
+            if( kPointers[minValueArrayIndex] >= list.get(minValueArrayIndex).length){
                 break;
+            }
+
+            //Calculate min/max for next round
+            max = Math.max(max, list.get(minValueArrayIndex)[kPointers[minValueArrayIndex]]);
+
+            minValueArrayIndex = 0;
+            min = list.get(0)[kPointers[0]];
+            for(int i=1; i<list.size(); i++){
+                if(list.get(i)[kPointers[i]] < min){
+                    min = list.get(i)[kPointers[i]];
+                    minValueArrayIndex = i;
+                }
             }
         }
 
@@ -72,7 +81,48 @@ public class MinRangeKSortedLists {
         int minRangeMin = -1;
         int minRangeMax = -1;
 
-        //TODO: Implementation
+        int [] kPointers = new int[list.size()];
+
+        int range,min,max;
+
+        max = list.get(0)[0];
+        for(int i=1; i<list.size(); i++){
+            if(list.get(i)[0] > max){
+                max = list.get(i)[0];
+            }
+        }
+
+        //MinHeap entry is a Pair:(number, array_index_which_contains_the_number)
+        //                          key             value
+        BinaryMinHeap<Pair<Integer, Integer>> binaryMinHeap = new BinaryMinHeap<>(list.size());
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).length>0) {
+                binaryMinHeap.insert(new Pair<>(list.get(i)[0], i));
+            }
+        }
+
+
+        while (true){
+            Pair pair = binaryMinHeap.extractMin();
+            min = (Integer) pair.getKey();
+            int minValueArrayIndex = (Integer) pair.getValue();
+
+            range = max-min;
+            if(minRange > range){
+                minRange = range;
+                minRangeMin = min;
+                minRangeMax = max;
+            }
+
+            kPointers[minValueArrayIndex]++;
+            if( kPointers[minValueArrayIndex] >= list.get(minValueArrayIndex).length){
+                break;
+            }
+
+            binaryMinHeap.insert(new Pair<>(list.get(minValueArrayIndex)[kPointers[minValueArrayIndex]], minValueArrayIndex));
+            // Check if the new element is greater than max
+            max = Math.max(max, list.get(minValueArrayIndex)[kPointers[minValueArrayIndex]]);
+        }
 
         return new int[]{minRangeMin, minRangeMax};
     }
@@ -91,5 +141,8 @@ public class MinRangeKSortedLists {
 
         int [] minRange = minRangeKSortedLists(list);
         System.out.println(minRange[0] + " -- " + minRange[1]);
+
+        int [] minRange1 = minRangeKSortedListsUsingBinaryMeanHeap(list);
+        System.out.println(minRange1[0] + " -- " + minRange1[1]);
     }
 }
