4536f9903372d9fff2301fc63bb863b927929d94
Bikas <anand.iitr.mca@gmail.com>
2019-11-19 10:49:51 +0530

    BuyAndSellStock to gain max profit.

---
+++ src/main/java/com/anand/coding/dsalgo/greedy/BuyAndSellStock.java
@@ -0,0 +1,48 @@
+package com.anand.coding.dsalgo.greedy;
+
+/**
+ * Given an array representing stock price for consecutive days.
+ * Determine max profit for a single buy-sell transaction.
+ *
+ * Eg:
+ * {7,1,5,3,6,4};
+ * Buy on day2  (price 1)
+ * Sell on day5 (price 6)
+ *
+ * Profit: 5
+ */
+public class BuyAndSellStock {
+
+    /**
+     *
+      * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+
+        if (prices.length == 0){
+            return 0;
+        }
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if ((prices[i] - buy) > 0) {
+                profit = Math.max(prices[i] - buy, profit);
+            } else {
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int [] prices = {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(prices));
+    }
+}
