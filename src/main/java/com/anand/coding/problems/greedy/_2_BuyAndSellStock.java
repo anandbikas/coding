3659d8febfe8c22ec5421d296d754b5387897cb3
Bikas Anand <anand.iitr.mca@gmail.com>
2020-12-02 23:56:48 +0530

    ByAndSell profit for multiple transactions.

--- src/main/java/com/anand/coding/problems/greedy/_2_BuyAndSellStock.java
+++ src/main/java/com/anand/coding/problems/greedy/_2_BuyAndSellStock.java
@@ -2,7 +2,7 @@ package com.anand.coding.problems.greedy;
 
 /**
  * Given an array representing stock price for consecutive days.
- * Determine max profit for a single buy-sell transaction.
+ * 1. Determine max profit for a single buy-sell transaction.
  *
  * Eg:
  * {7,1,5,3,6,4};
@@ -10,6 +10,18 @@ package com.anand.coding.problems.greedy;
  * Sell on day5 (price 6)
  *
  * Profit: 5
+ *
+ * 2. Determine max profit for multiple buy-sell transaction. Not greedy bu simple array calculation.
+ * Eg:
+ * {7,1,5,3,6,4};
+ * Buy on day2  (price 1)
+ * Sell on day3 (price 5)
+ *
+ * Buy on day4  (price 3)
+ * Sell on day5 (price 6)
+ *
+ * Profit: 4+3=7
+ *
  */
 public class _2_BuyAndSellStock {
 
@@ -18,7 +30,7 @@ public class _2_BuyAndSellStock {
       * @param prices
      * @return
      */
-    public static int maxProfit(int[] prices) {
+    public static int maxProfitSingleTransaction(int[] prices) {
 
         if (prices.length == 0){
             return 0;
@@ -36,6 +48,30 @@ public class _2_BuyAndSellStock {
         return profit;
     }
 
+
+    /**
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfitMultipleTransactions(int[] prices) {
+
+        int profit=0;
+
+        int buyAt=0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]<=prices[i-1]){
+                profit += prices[i-1] - prices[buyAt];
+                buyAt=i;
+            }
+        }
+        //Last transaction if any.
+        profit += prices[prices.length-1] - prices[buyAt];
+
+
+        return profit;
+    }
+
     /**
      *
      * @param args
@@ -43,6 +79,8 @@ public class _2_BuyAndSellStock {
     public static void main(String[] args) {
         int [] prices = {7,1,5,3,6,4};
 
-        System.out.println(maxProfit(prices));
+        System.out.println(maxProfitSingleTransaction(prices));
+        System.out.println(maxProfitMultipleTransactions(prices));
+
     }
 }
