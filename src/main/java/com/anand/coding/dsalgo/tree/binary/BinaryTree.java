990f1efac961ed378760430401a18cc6e763b1df
Bikas Anand <anand.iitr.mca@gmail.com>
2020-12-31 16:40:03 +0530

    BinaryTree: isSymmetric mirror image check.

--- src/main/java/com/anand/coding/dsalgo/tree/binary/BinaryTree.java
+++ src/main/java/com/anand/coding/dsalgo/tree/binary/BinaryTree.java
@@ -810,6 +810,38 @@ public class BinaryTree <T extends Comparable<T>> {
                 && isCopy(node1.right, node2.right);
     }
 
+    /**
+     * Checks whether the tree is mirror image of itself
+     *
+     * @param tree
+     * @return
+     */
+    public boolean isSymmetric(BinaryTree tree){
+        return root==null || isSymmetric(root.left, root.right);
+    }
+
+    /**
+     *
+     * @param node1
+     * @param node2
+     * @return
+     */
+    private boolean isSymmetric(Node node1, Node node2){
+        if(node1 == null && node2 == null){
+            return true;
+        }
+
+        if(node1 == null || node2 ==null){
+            return false;
+        }
+
+
+        return node1.data.equals(node2.data)
+                && isSymmetric(node1.left, node2.right)
+                && isSymmetric(node1.right, node2.left);
+    }
+
+
     private class NodePair{
         Node node1, node2;
 
