067428ea1da46267dc6795e197af353490571a72
Bikas Anand <anand.iitr.mca@gmail.com>
2021-01-31 02:38:56 +0530

    Graph Problem: AlienLanguageDictionary: Improve code.

--- src/main/java/com/anand/coding/problems/graph/_05_AlienLanguageDictionary.java
+++ src/main/java/com/anand/coding/problems/graph/_05_AlienLanguageDictionary.java
@@ -1,10 +1,5 @@
 package com.anand.coding.problems.graph;
-
-import com.anand.coding.dsalgo.graph.GraphType;
-import com.anand.coding.dsalgo.graph.adjacencylist.usingHashMap.Graph;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Find order of characters using sorted list of alien language words. Use Graph topological sorting.
@@ -18,7 +13,9 @@ public class _05_AlienLanguageDictionary {
      * @param words
      * @return
      */
-    public static List<Character> findAlphabetsInOrderUsingGenericGraph(String []words){
+    public static List<Character> findAlphabetsOrder(String []words){
+
+        HashMap<Character, Set<Character>> vertices = new HashMap<>();
 
         int n = words.length;
 
@@ -26,28 +23,61 @@ public class _05_AlienLanguageDictionary {
             return new ArrayList<>();
         }
 
-        Graph<Character> graph = new Graph(GraphType.DIRECTED);
-
+        // Create graph.
         for(int k=0; k<n-1; k++){
 
-            String word1 = words[k];
-            String word2 = words[k+1];
-
-            int i=0,j=0;
-            for(;i < word1.length() && j < word2.length(); i++,j++){
-
-               if(word1.charAt(i)!=word2.charAt(j)){
-                   graph.insert(word1.charAt(i));
-                   graph.insert(word2.charAt(j));
-
-                   graph.addEdge(word1.charAt(i), word2.charAt(j));
-
-                   break;
+            String w1=words[k], w2 = words[k+1];
+            for(int i=0 ;i < w1.length() && i < w2.length(); i++){
+                char u=w1.charAt(i), v=w2.charAt(i);
+               if(u!=v){
+                    if(!vertices.containsKey(u)){
+                        vertices.put(u, new HashSet<>());
+                    }
+                    if(!vertices.containsKey(v)){
+                        vertices.put(v, new HashSet<>());
+                    }
+                    vertices.get(u).add(v);
+                    break;
                }
             }
         }
 
-        return graph.topologicalSortingDfsRec();
+        //Topological Sorting
+
+        Queue<Character> queue = new ArrayDeque<>(vertices.size());
+        List<Character> topologicallySortedVertices = new ArrayList<>();
+
+        Map<Character,Integer> inDegrees = new HashMap<>();
+        for(Character u: vertices.keySet()) {
+            inDegrees.put(u, 0);
+        }
+
+        for(Character u: vertices.keySet()){
+            for(Character v: vertices.get(u)) {
+                inDegrees.put(v, inDegrees.get(v)+1);
+            }
+        }
+
+        for(Character u: vertices.keySet()){
+            if(inDegrees.get(u)==0){
+                queue.add(u);
+            }
+        }
+
+        while(!queue.isEmpty()){
+            Character u = queue.remove();
+            topologicallySortedVertices.add(u);
+
+            for(Character v: vertices.get(u)) {
+                //Reduce indegree once its parent is processed.
+                inDegrees.put(v,inDegrees.get(v)-1);
+                if(inDegrees.get(v) == 0){
+                    queue.add(v);
+                }
+            }
+        }
+
+        return topologicallySortedVertices;
     }
 
 
@@ -58,10 +88,9 @@ public class _05_AlienLanguageDictionary {
     public static void main(String [] args){
 
         String []wordList1 = {"baa", "abcd", "abca", "cab", "cad"};
-        System.out.println(findAlphabetsInOrderUsingGenericGraph(wordList1));
-
+        System.out.println(findAlphabetsOrder(wordList1));
 
         String [] wordList2 = {"caa", "aaa", "aab"};
-        System.out.println(findAlphabetsInOrderUsingGenericGraph(wordList2));
+        System.out.println(findAlphabetsOrder(wordList2));
     }
 }
