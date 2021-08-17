0a8275b34eb0becaf15da4ff4b4e6fd57c87c35b
Bikas Anand <anand.iitr.mca@gmail.com>
2021-08-17 22:10:46 +0530

    Heap: Sort a nearly sorted array using PriorityQueue.

--- src/main/java/com/anand/coding/problems/heap/_03_SortNearlySortedArray.java
+++ src/main/java/com/anand/coding/problems/heap/_03_SortNearlySortedArray.java
@@ -1,7 +1,7 @@
 package com.anand.coding.problems.heap;
 
-import com.anand.coding.dsalgo.array.Array;
-import com.anand.coding.dsalgo.tree.arraybased.BinaryMinHeap;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * In a nearly sorted array or k sorted array, element at each index gets position from i-k to i+k in the sorted list.
@@ -18,21 +18,18 @@ public class _03_SortNearlySortedArray {
      * @param k
      */
     public static void sortKSortedArray(int []A, int k){
-        BinaryMinHeap<Integer> binaryMinHeap = new BinaryMinHeap<>(k+1);
 
-        int i=0;
-        for(; i<=k; i++){
-            binaryMinHeap.insert(A[i]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k+1);
+
+        int i=0,j=0;
+        while(i<=k) pq.offer(A[i++]);
+
+        while (i<A.length){
+            A[j++] = pq.remove();
+            pq.offer(A[i++]);
         }
 
-        int j=0;
-        for(; i<A.length; i++){
-            A[j++] = binaryMinHeap.replace(0, A[i]);
-        }
-
-        while (!binaryMinHeap.isEmpty()) {
-            A[j++] = binaryMinHeap.extractMin();
-        }
+        while (!pq.isEmpty()) A[j++] = pq.poll();
     }
 
     /**
@@ -43,6 +40,6 @@ public class _03_SortNearlySortedArray {
        int []A = {6,5,3,2,8,10,9};
 
         sortKSortedArray(A,3);
-        Array.display(A);
+        System.out.println(Arrays.toString(A));
     }
 }
