696735b35dcaab5260a0cb3804a64ff849738eb5
Bikas Anand <anand.iitr.mca@gmail.com>
2021-08-26 04:12:55 +0530

    Code cleanup.

--- src/main/java/com/anand/coding/java/designpattern/creationalpattern/abstractfactorypattern/car/enums/Location.java
+++
@@ -1,9 +0,0 @@
-package com.anand.coding.java.designpattern.creationalpattern.abstractfactorypattern.car.enums;
-
-/**
- *
- */
-public enum Location {
-    INDIA,
-    USA
-}
--- src/main/java/com/anand/coding/java/designpattern/creationalpattern/builderpattern/meal/model/packing/Packing.java
+++
@@ -1,8 +0,0 @@
-package com.anand.coding.java.designpattern.creationalpattern.builderpattern.meal.model.packing;
-
-/**
- *
- */
-public interface Packing {
-    String pack();
-}
