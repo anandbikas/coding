3929fce1da327fe512eb87ab6d66e698227e05e5
Bikas Anand <anand.iitr.mca@gmail.com>
2021-09-03 01:24:36 +0530

    DisjointSet: IslandCountUsingDisjointSets code improvement and fix.

--- src/main/java/com/anand/coding/problems/disjointset/_02_IslandCountUsingDisjointSets.java
+++ src/main/java/com/anand/coding/problems/disjointset/_02_IslandCountUsingDisjointSets.java
@@ -1,6 +1,6 @@
 package com.anand.coding.problems.disjointset;
 
-import com.anand.coding.dsalgo.disjointset.DisjointUnionSets;
+import java.util.stream.IntStream;
 
 /**
  * Given a 2D matrix (0/1), find the number of islands.
@@ -19,18 +19,13 @@ import com.anand.coding.dsalgo.disjointset.DisjointUnionSets;
 public class _02_IslandCountUsingDisjointSets {
 
     // Possible 8 directions of a node.
-    static int[] row = { -1, -1, -1, 0, 0, 1, 1, 1 };
-    static int[] col = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    static int[] row = { -1, -1, -1,  0, 0,  1, 1, 1 };
+    static int[] col = { -1,  0,  1, -1, 1, -1, 0, 1 };
 
-    /**
-     *
-     * @param A
-     * @param n
-     * @param m
-     */
     public static int islandCount(int [][]A, int n, int m) {
 
-        DisjointUnionSets dus = new DisjointUnionSets(n*m);
+        int [] Parent = new int[n*m];
+        for(int i=0; i<Parent.length; Parent[i]=i++);
 
         int zerosCount = 0;
 
@@ -41,21 +36,30 @@ public class _02_IslandCountUsingDisjointSets {
 
                     // Process all 8 neighbors if any
                     for(int k=0; k<8; k++){
-
                         int p =  i+row[k];
                         int q =  j+col[k];
 
                         if(p<0 || p>=n || q<0 || q>=m || A[p][q]==0){
                             continue;
                         }
-                        dus.union(i*n+j, p*n+q);
+                        union(i*(m)+j, p*(m)+q, Parent); //Union
                     }
                 } else{
                     zerosCount++;
                 }
             }
         }
-        return dus.countSets()-zerosCount;
+
+        return (int)IntStream.range(0, Parent.length).filter(i -> Parent[i]==i).count() - zerosCount;  //Count
+    }
+
+    public static void union(int i, int j, int []Parent) {
+
+        int iRep, jRep;
+        for(iRep=i; iRep!=Parent[iRep]; iRep=Parent[iRep]); //Find
+        for(jRep=j; jRep!=Parent[jRep]; jRep=Parent[jRep]); //Find
+
+        Parent[iRep]=jRep;
     }
 
     /**
