a1ede962d97ca35826c7690413bc1aad81d74cd9
Bikas Anand <anand.iitr.mca@gmail.com>
2022-11-21 00:04:43 +0530

    FloodFill: Code improvement

--- src/main/java/com/anand/coding/problems/graph/_01_FloodFill.java
+++ src/main/java/com/anand/coding/problems/graph/_01_FloodFill.java
@@ -1,7 +1,7 @@
 package com.anand.coding.problems.graph;
 
 /**
- * Flood fill is used in MS paint to fill the pixel along with all surrounding same colored pixels with another color using DFS algorithm.
+ * Flood fill is used in MS Paint to fill the pixel along with all surrounding same colored pixels with another color using DFS algorithm.
  *
  * Input:
  *        screen[M][N] ={{1, 1, 1, 1, 1},
@@ -27,44 +27,26 @@ public class _01_FloodFill {
 
     // Possible 8 directions of a node.
     // 4 directions can also be used.
-    static int[] row = { -1, -1, -1, 0, 0, 1, 1, 1 };
-    static int[] col = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    static int[] R = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    static int[] C = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    static int[] R4 = { -1, 0, 0, 1}, C4 = { 0, -1, 1, 0};
 
-    /**
-     *
-     * @param A
-     * @param n
-     * @param m
-     * @param i
-     * @param j
-     * @param newColor
-     */
-    public static void floodFill(int [][]A, int n, int m, int i, int j, int newColor) {
-        floodFill(A, n, m, i, j, A[i][j], newColor);
-
+    public static void floodFill(int [][]A, int i, int j, int newColor) {
+        int n=A.length, m=A[0].length;
+        floodFill(A, n, m, i, j, A[i][j], newColor, new boolean[n][m]);
     }
 
-    /**
-     *
-     * @param A
-     * @param n
-     * @param m
-     * @param i
-     * @param j
-     * @param oldColor
-     * @param newColor
-     */
-    private static void floodFill(int [][]A, int n, int m, int i, int j, int oldColor, int newColor) {
+    private static void floodFill(int [][]A, int n, int m, int i, int j, int oldColor, int newColor, boolean[][] visited) {
 
-        if(i<0 || i>=n || j<0 || j>=m || A[i][j]==0 || A[i][j]!=oldColor){
+        if(i<0 || i>=n || j<0 || j>=m || visited[i][j] || A[i][j]==0 || A[i][j]!=oldColor){
             return;
         }
 
         A[i][j] = newColor;
 
         // DFS all 8 neighbors if any
-        for(int k=0; k<8; k++){
-            floodFill(A, n, m, i+row[k], j+col[k], oldColor, newColor);
+        for(int k=0; k<8; k++) {
+            floodFill(A, n, m, i+R[k], j+C[k], oldColor, newColor, visited);
         }
     }
 
@@ -72,27 +54,23 @@ public class _01_FloodFill {
      *
      * @param args
      */
-    public static void main(String []args){
+    public static void main(String []args) {
         int [][]A = {{1, 1, 1, 1, 1},
-                    {1, 1, 1, 1, 1},
-                    {1, 0, 0, 1, 1},
-                    {1, 2, 2, 2, 2},
-                    {1, 1, 1, 2, 2},
-                    {1, 1, 1, 2, 1},
-        };
+                     {1, 1, 1, 1, 1},
+                     {1, 0, 0, 1, 1},
+                     {1, 2, 2, 2, 2},
+                     {1, 1, 1, 2, 2},
+                     {1, 1, 1, 2, 1}};
 
-        floodFill(A, 6,5,4,3, 5);
-
-        display(A,6,5);
+        floodFill(A,4,3, 5);
+        display(A);
     }
 
-    public static void display(int [][] A, int n, int m)
-    {
+    public static void display(int [][] A) {
         System.out.println();
-        for(int i=0; i<n; i++){
-
-            for(int w =0; w<m; w++){
-                System.out.print(String.format("%4d", A[i][w]));
+        for(int i=0; i<A.length; i++){
+            for(int j =0; j<A[0].length; j++){
+                System.out.printf("%4d", A[i][j]);
             }
             System.out.println();
         }
