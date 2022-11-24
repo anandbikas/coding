da6ec134250544a2fe7f9fa1b9445050fbb309cb
Bikas Anand <anand.iitr.mca@gmail.com>
2022-11-25 03:43:27 +0530

    AlienLanguageDictionary: 1. Find alphabet order 2. Validate given words are sorted.

--- src/main/java/com/anand/coding/problems/graph/_05_AlienLanguageDictionary.java
+++ src/main/java/com/anand/coding/problems/graph/_05_AlienLanguageDictionary.java
@@ -2,21 +2,15 @@ package com.anand.coding.problems.graph;
 import java.util.*;
 
 /**
- * Find order of characters using sorted list of alien language words. Use Graph topological sorting.
- *
+ * 1. Find order of characters using sorted list of alien language words. Use Graph topological sorting.
+ * 2. Given order or characters, Validate whether words are sorted.
  */
 public class _05_AlienLanguageDictionary {
 
 
-    /**
-     *
-     * @param words
-     * @return
-     */
     public static List<Character> findAlphabetsOrder(String []words){
 
         HashMap<Character, Set<Character>> vertices = new HashMap<>();
-
         int n = words.length;
 
         if(n<=1){
@@ -25,9 +19,9 @@ public class _05_AlienLanguageDictionary {
 
         // Create graph.
         for(int k=0; k<n-1; k++){
-
             String w1=words[k], w2 = words[k+1];
-            for(int i=0 ;i < w1.length() && i < w2.length(); i++){
+
+            for(int i=0; i<w1.length() && i<w2.length(); i++){
                 char u=w1.charAt(i), v=w2.charAt(i);
                if(u!=v){
                     if(!vertices.containsKey(u)){
@@ -43,9 +37,8 @@ public class _05_AlienLanguageDictionary {
         }
 
         //Topological Sorting
-
         Queue<Character> queue = new ArrayDeque<>(vertices.size());
-        List<Character> topologicallySortedVertices = new ArrayList<>();
+        List<Character> sortedVertices = new ArrayList<>();
 
         Map<Character,Integer> inDegrees = new HashMap<>();
         for(Character u: vertices.keySet()) {
@@ -58,18 +51,14 @@ public class _05_AlienLanguageDictionary {
             }
         }
 
-        for(Character u: vertices.keySet()){
-            if(inDegrees.get(u)==0){
-                queue.add(u);
-            }
-        }
+        vertices.keySet().stream().filter(u->inDegrees.get(u)==0).forEach(queue::add);
 
         while(!queue.isEmpty()){
             Character u = queue.remove();
-            topologicallySortedVertices.add(u);
+            sortedVertices.add(u);
 
             for(Character v: vertices.get(u)) {
-                //Reduce indegree once its parent is processed.
+                //Reduce inDegree once its parent is processed.
                 inDegrees.put(v,inDegrees.get(v)-1);
                 if(inDegrees.get(v) == 0){
                     queue.add(v);
@@ -77,9 +66,28 @@ public class _05_AlienLanguageDictionary {
             }
         }
 
-        return topologicallySortedVertices;
+        return sortedVertices;
     }
 
+    public static boolean isAlienWordsSorted(String[] words, List<Character> order) {
+
+        Map<Character, Integer> orderMap = new HashMap<>();
+        for(int i=0; i<order.size(); i++){
+            orderMap.put(order.get(i), i);
+        }
+
+        for(int k=0; k<words.length-1; k++){
+            String x = words[k], y = words[k+1];
+
+            int i=0;
+            for(; i<x.length() && i<y.length() && x.charAt(i)==y.charAt(i); i++);
+
+            if(i<x.length() && (i==y.length() || orderMap.get(x.charAt(i))>orderMap.get(y.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
 
     /**
      *
@@ -89,8 +97,10 @@ public class _05_AlienLanguageDictionary {
 
         String []wordList1 = {"baa", "abcd", "abca", "cab", "cad"};
         System.out.println(findAlphabetsOrder(wordList1));
+        System.out.println(isAlienWordsSorted(wordList1, findAlphabetsOrder(wordList1)));
 
         String [] wordList2 = {"caa", "aaa", "aab"};
         System.out.println(findAlphabetsOrder(wordList2));
+        System.out.println(isAlienWordsSorted(wordList2, findAlphabetsOrder(wordList2)));
     }
 }
