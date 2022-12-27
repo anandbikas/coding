e1868c49321041e2d2052cdc6fbca16c87d96b95
Bikas Anand <anand.iitr.mca@gmail.com>
2022-12-27 23:02:01 +0530

    DisjointSet: Island Count

--- src/main/java/com/anand/coding/problems/graph/disjointset/_01_IslandCountDisjointSets.java
+++ src/main/java/com/anand/coding/problems/graph/disjointset/_01_IslandCountDisjointSets.java
@@ -1,10 +1,10 @@
-package com.anand.coding.problems.disjointset;
+package com.anand.coding.problems.graph.disjointset;
 
 import java.util.stream.IntStream;
 
 /**
  * Given a 2D matrix (0/1), find the number of islands.
- * A group of connected 1s forms an island
+ * A group of connected 1's forms an island
  *
  * Input : mat[][] = {{1, 1, 0, 0, 0},
  *                    {0, 1, 0, 0, 1},
@@ -13,14 +13,14 @@ import java.util.stream.IntStream;
  *                    {1, 0, 1, 0, 1}
  * Output : 5
  *
- * This can be solved using DisjointSets as well, refer IslandCountUsingDfs.java
+ * DisjointSets solution: Refer IslandCountDFS.java
  *
  */
-public class _02_IslandCountUsingDisjointSets {
+public class _01_IslandCountDisjointSets {
 
-    // Possible 8 directions of a node.
-    static int[] row = { -1, -1, -1,  0, 0,  1, 1, 1 };
-    static int[] col = { -1,  0,  1, -1, 1, -1, 0, 1 };
+    // Possible directions of a node up, down, left, right.
+    static int[] R = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    static int[] C = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
     public static int islandCount(int [][]A, int n, int m) {
 
@@ -30,27 +30,28 @@ public class _02_IslandCountUsingDisjointSets {
         int zerosCount = 0;
 
         //Process each node
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(A[i][j]==1) {
-
-                    // Process all 8 neighbors if any
-                    for(int k=0; k<8; k++){
-                        int p =  i+row[k];
-                        int q =  j+col[k];
-
-                        if(p<0 || p>=n || q<0 || q>=m || A[p][q]==0){
-                            continue;
-                        }
-                        union(i*(m)+j, p*(m)+q, Parent); //Union
-                    }
-                } else{
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(A[i][j]==0) {
                     zerosCount++;
+                    continue;
+                }
+
+                // BFS all neighbors if any
+                for(int k=0; k<R.length; k++){
+                    int p =  i+R[k];
+                    int q =  j+C[k];
+
+                    if(p<0 || p>=n || q<0 || q>=m || A[p][q]==0){
+                        continue;
+                    }
+                    union((i*m)+j, (p*m)+q, Parent); //Union
                 }
             }
         }
 
-        return (int)IntStream.range(0, Parent.length).filter(i -> Parent[i]==i).count() - zerosCount;  //Count
+        return (int)IntStream.range(0, Parent.length)
+                .filter(i -> Parent[i]==i).count() - zerosCount;
     }
 
     public static void union(int i, int j, int []Parent) {
