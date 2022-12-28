3cd17068b3e05187912750378b9823a4877a50d5
Bikas Anand <anand.iitr.mca@gmail.com>
2022-12-29 02:20:53 +0530

    DP: EggDrop code improvement.

--- src/main/java/com/anand/coding/problems/dp/_06_EggDrop.java
+++ src/main/java/com/anand/coding/problems/dp/_06_EggDrop.java
@@ -24,180 +24,116 @@ package com.anand.coding.problems.dp;
  *
  *    -> Iterative tabulation approach.
  *
- *  Ref: https://brilliant.org/wiki/egg-dropping/
+ *  Ref: brilliant.org/wiki/egg-dropping/
+ *       leetcode.com/problems/super-egg-drop/solutions/158936/super-egg-drop/
  */
 public class _06_EggDrop {
 
+    /**
+     * For n=2 eggs, k floor.
+     */
+    public int twoEggDrop(int k) {
+        return (int)Math.ceil((-1 + Math.sqrt(1+8*k)) / 2.0);
+    }
 
     /**
      *  Recursive approach with memoization.
-     *
-     * @param n
-     * @param k
-     * @return
      */
     public static int eggDrop(int n, int k){
 
-        int DP[][] = new int[n+1][k+1];   //Egg-Floor
+        int[][] DP = new int[n+1][k+1];   //Egg-Floor
 
         return eggDrop(n,k,DP);
     }
 
     private static int eggDrop(int n, int k, int [][]DP) {
 
-        // 0 trials for no floor
-        // 1 trials for one floor.
-        if (k == 1 || k == 0) {
-            return k;
-        }
+        if (k == 1 || k == 0) return k; // 0 trials for no floor, 1 trials for one floor
+        if (n == 1) return k;           // k trials for one egg and k floors
 
-        // k trials for one egg and k floors
-        if (n == 1) {
-            return k;
-        }
-
-        if(DP[n][k]>1){
+        if(DP[n][k]>1) {
             return DP[n][k];
         }
 
-//        // Approach 1:
-//        int minTrials = Integer.MAX_VALUE;
-//
-//        // Consider all droppings from 1st floor to jth floor and find minimum trials.
-//        for (int x = 1; x <= k; x++) {
-//
-//            //trails in left side if breaks
-//            int left    = DP[n-1][x-1];                 //one egg and one floor gone till now.
-//
-//            //trails in right side if does not break
-//            int right   = DP[n][k-x];               //x floors gone till now.
-//
-//            int trials = Math.max(left, right)+1;
-//
-//            if(trials<minTrials){
-//                minTrials = trials;
-//            }
-//        }
-//        return DP[n][k] = minTrials;
-
-
-        //Approach 2: Binary search
-        int low=1;
-        int high = k;
+        //Approach: Binary search
         int minTrials = k;
-        while(low < high) {
-            int midFloor = low + (high-low)/2;
+        for(int low=1,high=k; low<high;) {
+            int mid = low + (high-low)/2;
 
-            //trails in left side if breaks
-            int left = eggDrop(n-1, midFloor-1, DP);    //one egg and one floor gone till now.
+            //trails on the left side if breaks
+            int left = eggDrop(n-1, mid-1, DP);     // one egg gone till now.
 
-            //trails in right side if does not break
-            int right = eggDrop(n, k-midFloor, DP);         //x(midFloor) floors gone till now.
+            //trails on the right side if it does not break
+            int right = eggDrop(n, k-mid, DP);         // x(midFloor) floors gone till now.
 
-            int trials = Math.max(left,right) +1;
+            int trials = Math.max(left,right) + 1;
 
-            if(trials<minTrials){
+            if(trials<minTrials) {
                 minTrials = trials;
             }
 
-            if(left==right){
+            if(left==right) {
                 break;
-            } else if(left<right){
-                low = midFloor+1;
+            } else if(left<right) {
+                low = mid+1;
             } else {
-                high = midFloor;    // here, the midFloor also required as left side calculation had
-                // considered this floor lost/broke one egg.
-                // So next calculation will consider this floor with all n eggs remaining.
+                high = mid;     // here, the midFloor also required as left side calculation had
+                                // considered this floor lost/broke one egg.
+                                // So next calculation will consider this floor with all n eggs remaining.
             }
         }
+
         return DP[n][k] = minTrials;
     }
 
 
     /**
      *  Iterative approach with tabulation.
-     *
-     * @param n
-     * @param k
-     * @return
      */
-    public static int eggDropIterative(int n, int k){
+    public static int eggDropIterative(int n, int k) {
 
-        if (k == 1 || k == 0) {
-            return k;
-        }
-
-        if (n == 1) {
-            return k;
-        }
+        if (k == 1 || k == 0) return k;
+        if (n == 1) return k;
 
         int [][] DP = new int[n+1][k+1];    //Egg-Floor
 
-        // 0 trials for no floor
-        // 1 trials for one floor.
-        for(int i=0; i<=n; i++){
-            DP[i][0] = 0;
-            DP[i][1] = 1;
+        for(int i=0; i<=n; i++) {
+            DP[i][0] = 0; // 0 trials for zero floor
+            DP[i][1] = 1; // 1 trials for one floor
+        }
+        for(int j=1; j<=k; j++) {
+            DP[1][j] = j; // k trials for one egg and k floors
         }
 
-        // k trials for one egg and k floors
-        for(int j=1; j<=k; j++){
-            DP[1][j] = j;
-        }
+        for(int i=2; i<=n; i++) { //Calculate for 2 eggs onwards
+            for(int j=2; j<=k; j++) { //Calculate for 2nd floors onwards
 
-        //Calculate for 2 eggs onwards.
-        for(int i=2; i<=n; i++) {
-            //Calculate for 2nd floors onwards
-            for(int j=2; j<=k; j++){
-
-//                //Approach 1:
-//                int minTrials = Integer.MAX_VALUE;
-//
-//                // Consider all droppings from 1st floor to jth floor and find minimum trials.
-//                for (int x = 1; x <= j; x++) {
-//
-//                    //trails in left side if breaks
-//                    int left    = DP[i-1][x-1];                     //one egg and one floor gone till now.
-//
-//                    //trails in right side if does not break
-//                    int right   = DP[i][j-x];                       //x floors gone till now.
-//
-//                    int trials = Math.max(left, right) + 1;
-//
-//                    if(trials<minTrials){
-//                        minTrials = trials;
-//                    }
-//                }
-//                DP[i][j] = minTrials;
-//
-
-                //Approach 2: Binary search
-                int low=1;
-                int high = j;
+                //Approach: Binary search
                 int minTrials = j;
-                while(low < high) {
-                    int midFloor = low + (high-low)/2;
+                for(int low=1,high=j; low<high;) {
+
+                    int mid = low + (high-low)/2;
 
                     //trails in left side if breaks
-                    int left = DP[i-1][midFloor-1];                 //one egg and one floor gone till now.
+                    int left = DP[i-1][mid-1];                 // one egg gone till now.
 
-                    //trails in right side if does not break
-                    int right = DP[i][j-midFloor];                  //x(midFloor) floors gone till now.
+                    //trails in right side if it does not break
+                    int right = DP[i][j-mid];                  // x(midFloor) floors gone till now.
 
-                    int trials = Math.max(left,right) +1;
+                    int trials = Math.max(left,right) + 1;
 
-                    if(trials<minTrials){
+                    if(trials<minTrials) {
                         minTrials = trials;
                     }
 
                     if(left==right){
                         break;
                     } else if(left<right){
-                        low = midFloor+1;
+                        low = mid+1;
                     } else {
-                        high = midFloor;    // here, the midFloor also required as left side calculation had
-                        // considered this floor lost/broke one egg.
-                        // So next calculation will consider this floor with all n eggs remaining.
+                        high = mid;     // here, the midFloor also required as left side calculation had
+                                        // considered this floor lost/broke one egg.
+                                        // So next calculation will consider this floor with all n eggs remaining.
                     }
                 }
                 DP[i][j] = minTrials;
@@ -211,15 +147,11 @@ public class _06_EggDrop {
      *
      * @param args
      */
-    public static void main(String args[])
-    {
+    public static void main(String[] args) {
         int n = 2;
-        // int k = 10;
 
-        for(int k=0; k<33; k++) {
-            System.out.println(k + " -> " + eggDrop(n, k));
-            System.out.println(k + " -> " + eggDropIterative(n, k));
-            System.out.println();
+        for(int k=0; k<=33; k++) {
+            System.out.println(k + " -> " + eggDrop(n, k) + ", " + eggDropIterative(n, k));
         }
     }
 }
