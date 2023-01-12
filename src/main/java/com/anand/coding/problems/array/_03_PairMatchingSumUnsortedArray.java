4f5b5fdbacc34197bf9dc6ce3229c6dea77823ec
Bikas Anand <anand.iitr.mca@gmail.com>
2023-01-13 01:53:05 +0530

    Array: Pair Sum.

--- src/main/java/com/anand/coding/problems/array/_03_PairMatchingSumUnsortedArray.java
+++ src/main/java/com/anand/coding/problems/array/_03_PairMatchingSumUnsortedArray.java
@@ -1,6 +1,7 @@
 package com.anand.coding.problems.array;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Find pair with given sum in an unsorted array. One number should be possibly the largest.
@@ -8,78 +9,37 @@ import java.util.*;
  */
 public class _03_PairMatchingSumUnsortedArray {
 
-    /**
-     * Using sort
-     * Complexity: O(n Log(n))
-     *
-     * @param sum
-     * @param list
-     * @return
-     */
-    public static int[] pairSum(int sum, ArrayList<Integer> list){
+    public static int[] pairSum(int []A, int target){
 
-        ArrayList<Integer> list1 = (ArrayList<Integer>) list.clone();
-        Collections.sort(list1);
+        List<Integer> list = Arrays.stream(A).boxed().collect(Collectors.toList());
+        Arrays.sort(A);
 
-        for(int i=0, j=list1.size()-1; i<j ;) {
-
-            int pairSum = list1.get(i)+list1.get(j);
-            if( pairSum == sum) {
-                int[] res = {list.indexOf(list1.get(i)), list.indexOf(list1.get(j))};
+        for(int i=0, j=A.length-1; i<j;) {
+            int sum = A[i]+A[j];
+            if(sum == target) {
+                int [] res = new int[]{list.indexOf(A[i]),list.lastIndexOf(A[j])};
                 Arrays.sort(res);
                 return res;
             }
 
-            if (pairSum < sum) i++; else j--;
+            if (sum > target) j--; else i++;
         }
 
         return new int[]{-1,-1};
     }
 
-    /**
-     * using HashMap
-     * Complexity: O(n)
-     *
-     * @param sum
-     * @param list
-     * @return
-     */
-    public static int[] pairSumUsingHashMap(int sum, ArrayList<Integer> list){
+    public static int[] pairSumUsingHashMap(int []A, int target){
+        Map<Integer, Integer> indexMap = new HashMap<>();
 
-        int index1=-1,index2=-1;
-
-        Map<Integer, List<Integer>> indexMap = new HashMap<>();
-
-        for(int i=0; i<list.size(); i++){
-            int value = list.get(i);
-            if(!indexMap.containsKey(value)){
-                indexMap.put(value, new ArrayList<>());
+        for(int i=0; i<A.length; i++) {
+            int diff = target-A[i];
+            if(indexMap.containsKey(diff)){
+                return new int[]{indexMap.get(diff), i};
             }
-            indexMap.get(value).add(i);
+            indexMap.put(A[i], i);
         }
 
-        // Workaround for n1==n2
-        if(sum%2==0){
-            int n=sum/2;
-            if(indexMap.containsKey(n) && indexMap.get(n).size()>1) {
-                index1 = indexMap.get(n).get(0);
-                index2 = indexMap.get(n).get(1);
-                indexMap.remove(n);
-            }
-        }
-
-        for(int n1: indexMap.keySet()) {
-            int n2 = sum-n1;
-
-            if(indexMap.containsKey(n2)) {
-                if(index1==-1 || Math.max(list.get(index1), list.get(index2))<Math.max(n1, n2)) {
-                    index1 = indexMap.get(n1).get(0);
-                    index2 = indexMap.get(n2).get(0);
-                }
-            }
-        }
-
-        return new int[]{index1, index2};
+        return new int[]{-1,-1};
     }
 
     /**
@@ -88,16 +48,7 @@ public class _03_PairMatchingSumUnsortedArray {
      */
     public static void main(String []args){
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-        list.add(100);
-        list.add(180);
-        list.add(40);
-        list.add(120);
-        list.add(10);
-
-
-        System.out.println(Arrays.toString(pairSum(220, list)));
-        System.out.println(Arrays.toString(pairSumUsingHashMap(220, list)));
+        System.out.println(Arrays.toString(pairSum(new int[] {100,180,40,120,10}, 220)));
+        System.out.println(Arrays.toString(pairSumUsingHashMap(new int[] {100,180,40,120,10}, 220)));
     }
 }
--- src/main/java/com/anand/coding/problems/array/_04_AllPairsMatchingSumUnsortedArray.java
+++ src/main/java/com/anand/coding/problems/array/_04_AllPairsMatchingSumUnsortedArray.java
@@ -9,72 +9,53 @@ import java.util.*;
  */
 public class _04_AllPairsMatchingSumUnsortedArray {
 
-    /**
-     * Complexity O(n log n) using binarySearch
-     *
-     * @param A
-     * @param sum
-     * @return
-     */
-    public static List<int []> findAllPairsWithMatchingSumNLogN(int []A, final int sum){
+    public static List<int []> findAllPairsMatchingSum(int []A, final int target){
 
         Arrays.sort(A);
-        List<int []> list = new ArrayList<>();
+        List<int []> res = new ArrayList<>();
 
-        for(int i=0; i<A.length-1; i++){
-            int j = Arrays.binarySearch(A, i+1, A.length, sum-A[i]);
-            if( j >= 0){
-                list.add(new int[]{A[i],A[j]});
+        for(int i=0, j=A.length-1; i<j;) {
+            int sum = A[i]+A[j];
+            if(sum == target) {
+                res.add(new int[]{A[i],A[j]});
             }
+
+            if (sum > target) j--; else i++;
         }
-        return list;
+
+        return res;
     }
 
-    /**
-     * using HashMap
-     *
-     * Complexity: O(n)
-     *
-     * @param A
-     * @param sum
-     * @return
-     */
-    public static List<int []> findAllPairsWithMatchingSumUsingHashMap(int []A, final int sum){
+    public static List<int []> findAllPairsMatchingSumUsingHashMap(int []A, final int target){
 
-        List<int []> list = new ArrayList<>();
+        List<int []> res = new ArrayList<>();
 
         //list takes care for duplicate values
         Map<Integer, ArrayList<Integer>> indexMap = new HashMap<>();
 
         for(int i=0; i<A.length; i++){
-            int value = A[i];
-            if(!indexMap.containsKey(value)){
-                indexMap.put(value, new ArrayList<>());
-            }
-            indexMap.get(value).add(i);
+            indexMap.computeIfAbsent(A[i], k->new ArrayList<>()).add(i);
         }
 
-        for(int i=0; i<A.length; i++){
-            int n1 = A[i];
-            int n2 = sum-A[i];
+        for (int n1 : A) {
+            int n2 = target - n1;
 
-            if(indexMap.containsKey(n2)){
+            if (indexMap.containsKey(n2)) {
 
-                for(int n1Index: indexMap.get(n1)) {
-                    for (int n2Index : indexMap.get(n2)) {
+                for (int i : indexMap.get(n1)) {
+                    for (int j : indexMap.get(n2)) {
                         // Skip if same index is found.
-                        if (n1Index == n2Index) {
-                            continue;
-                        }
+                        if (i == j) continue;
+
                         // Index can also be added depending on requirement.
-                        list.add(new int[]{n1, n2});
+                        res.add(new int[]{n1,n2});
                     }
                 }
                 indexMap.remove(n1);
                 indexMap.remove(n2);
             }
         }
-        return list;
+        return res;
     }
 
     /**
@@ -83,18 +64,15 @@ public class _04_AllPairsMatchingSumUnsortedArray {
      */
     public static void main(String [] args){
 
-        int A[] = new int[]{7, 3, 2, 5, 3, 1, 6, 4};
+        int []A = new int[]{7, 3, 2, 5, 3, 1, 6, 4};
 
-        int matchingSum =9;
-        List<int []> matchingSumPairs = findAllPairsWithMatchingSumNLogN(A, matchingSum);
-        System.out.println("matchingSumPairs for sum  = " + matchingSum);
-        matchingSumPairs.forEach(Array::display);
-        System.out.println();
+        List<int []> res = findAllPairsMatchingSum(A, 9);
+        System.out.println("matchingSumPairs for sum  = " + 9);
+        res.forEach(Array::display);
 
-        matchingSumPairs = findAllPairsWithMatchingSumUsingHashMap(A, matchingSum);
-        System.out.println("matchingSumPairs for sum  = " + matchingSum);
-        matchingSumPairs.forEach(Array::display);
-        System.out.println();
-
+        int []B = new int[]{7, 3, 2, 5, 3, 1, 6, 4};
+        res = findAllPairsMatchingSumUsingHashMap(B, 9);
+        System.out.println("matchingSumPairs for sum  = " + 9);
+        res.forEach(Array::display);
     }
 }
