9cbe38575f82ce087d185bf8415cea4b5607623a
Bikas Anand <anand.iitr.mca@gmail.com>
2023-04-25 03:06:37 +0530

    Backtracking: Partition Equally K subsets, optimised by sorting and fast-tracking for equal values.

--- src/main/java/com/anand/coding/problems/backtracking/_01_PartitionEquallyKSubsets.java
+++ src/main/java/com/anand/coding/problems/backtracking/_01_PartitionEquallyKSubsets.java
@@ -4,39 +4,40 @@ import java.util.Arrays;
 
 /**
  * Check if an array of positive integers be partitioned into k equal subsets?
- *
  * Backtracking.
- * Also see DP knapsack solution _01_Knapsack_0_1_PartitionEquallyKSubsets
- *
+ * /problems/partition-to-k-equal-sum-subsets
  */
 public class _01_PartitionEquallyKSubsets {
 
     public static boolean canPartitionEquallyKSubsetsBacktracking(int[] nums, int k) {
         int sum = Arrays.stream(nums).sum();
-        if(sum==0 || sum%k!=0){
+        if(sum==0 || sum%k!=0) {
             return false;
         }
-        return findPartitions(nums, new boolean[nums.length],sum/k,0,0, k);
+        Arrays.sort(nums);
+
+        return findPartitions(nums, 0, 0, sum/k, new boolean[nums.length], k);
     }
 
-    private static boolean findPartitions(int [] nums, boolean []visited, int target, int currSum, int i, int k){
-        if(k==1){
+    private static boolean findPartitions(int [] nums, int i, int currSum, int target, boolean []visited, int k) {
+        if(k==1) {
             return true;
         }
 
         if(currSum==target) {
-            return findPartitions(nums, visited, target, 0, 0, k-1);
+            return findPartitions(nums, 0, 0, target, visited, k-1);
         }
 
-        for(int j=i;j<nums.length;j++){
-            if(visited[j] || currSum+nums[j]>target) {
-                continue;
-            }
+        for(int j=i; j<nums.length; j++) {
+            if(visited[j] )             continue;
+            if(currSum+nums[j]>target)  return false; //Because next are all the greater values
+
             visited[j]=true;
-            if(findPartitions(nums, visited, target, currSum + nums[j], j+1, k)) {
+            if(findPartitions(nums, j+1, currSum + nums[j], target, visited, k)) {
                 return true;
             }
             visited[j]=false;
+            while(j<nums.length-1 && nums[j]==nums[j+1]) j++; // If sorted, we can optimise by fast-tracking for same value.
         }
         return false;
     }
@@ -52,7 +53,9 @@ public class _01_PartitionEquallyKSubsets {
         System.out.println(canPartitionEquallyKSubsetsBacktracking(new int[]{1,2,2,3,3,4,5}, 4));
 
         System.out.println();
-
         System.out.println(canPartitionEquallyKSubsetsBacktracking(new int[]{1,2,3,5}, 2));
+
+        System.out.println();
+        System.out.println(canPartitionEquallyKSubsetsBacktracking(new int[]{10,10,7,8,10,4,9,7,9,10,4,6,7,1,8,5}, 5));
     }
 }
